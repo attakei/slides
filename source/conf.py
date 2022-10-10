9a488cd482aaed450e1d9f379f38b9555b5ebc84
Kazuya Takei <myself@attakei.net>
2022-10-11 01:38:23 +0900

    feat: Use sphinxcontrib-sass and setting up standard theme

--- source/conf.py
+++ source/conf.py
@@ -2,6 +2,7 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+from sphinx_revealjs.themes import get_theme_path
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -16,6 +17,7 @@ release = "2022.10.11"
 
 extensions = [
     "sphinx_revealjs",
+    "sphinxcontrib.sass",
 ]
 
 templates_path = ["_templates"]
@@ -28,3 +30,33 @@ language = "ja"
 
 html_theme = "alabaster"
 html_static_path = ["_static"]
+
+# -- Options for REVEALJS output
+revealjs_style_theme = "css/my-solarized.css"
+revealjs_static_path = ["_static"]
+revealjs_css_files = [
+    "revealjs4/plugin/highlight/zenburn.css",
+]
+revealjs_script_conf = {
+    "controls": False,
+    "hash": True,
+    "center": False,
+    "transition": "none",
+}
+revealjs_script_plugins = [
+    {
+        "src": "revealjs4/plugin/highlight/highlight.js",
+        "name": "RevealHighlight",
+    },
+]
+
+# -- Options for extensions
+# sphinxcontrib-sass
+sass_src_dir = "_sass"
+sass_out_dir = "_static/css"
+sass_include_paths = [
+    get_theme_path("sphinx_revealjs") / "static/revealjs4/css/theme",
+]
+sass_targets = {
+    "my-solarized.scss": "my-solarized.css",
+}
